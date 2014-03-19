8574b35f5bdc514b12b75268b54a02e6983cf04c
ScottishDave <dmckeown@bgtpartners.com>
2014-03-19 14:48:11 -0400

    Updating linting path

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,7 +10,7 @@ module.exports = function (Grunt) {
             options: {
                 jshintrc: '.jshintrc'
             }
-            , files: 'src/Vimg.js'
+            , files: 'src/*.js'
         }
 
         , concat: {
